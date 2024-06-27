613c465017f5a8c10516354d8af68f9a4f66699c
koiw1202 <koiw1202@naver.com>
2024-06-27 23:45:00 +0900

    문자열 테스트

--- src/string/Test.java
+++ src/string/Test.java
@@ -9,8 +9,19 @@ package string;
  */
 public class Test {
 
-    public static void main(String args[]){
+    public static String transFormTest(String s) {
+        return "transFormTest" ;
+    }
 
+    public static void main(String args[]){
+        String s = "123" ;
+        System.out.println(s.transform(Test::transFormTest)) ;
+        String output = """
+                        Name: %s
+                        Phone: %s
+                        """.formatted("test", "01012341234") ;
+
+        System.out.println(output);
     }
 
 }
