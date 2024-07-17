bdc094681e50c98a7480757bc9b730849abe8157
koiw1202 <koiw1202@naver.com>
2024-07-17 23:54:05 +0900

    enum 테스트

---
+++ src/enumTest/Main.java
@@ -0,0 +1,15 @@
+package enumTest;
+
+/**
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-07-17        koiw1       최초 생성
+ */
+public class Main {
+
+    public static void main(String args[]) {
+
+    }
+}
