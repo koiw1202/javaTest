ffd001e23babf50220aea07eaf77aedddb3f43f5
koiw1202 <koiw1202@naver.com>
2024-12-27 23:26:22 +0900

    연산자 성능 테스트

---
+++ src/speedTest/BitOperatorTest.java
@@ -0,0 +1,37 @@
+package speedTest;
+
+/**
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-12-27        koiw1       최초 생성
+ */
+public class BitOperatorTest {
+
+//    public static void main(String argsp[]) {
+//
+//        long stTime = System.currentTimeMillis();
+//
+//        int[] arr = new int[1000000000];
+//
+//        for(long i = 0 ; i < 1000000000; i++) {
+//            arr[(int) i] = 3/2;
+//        }
+//        long endTime = System.currentTimeMillis();
+//        System.out.println( (double) (endTime - stTime) / 1000 );
+//    }
+
+    public static void main(String argsp[]) {
+
+        long stTime = System.currentTimeMillis();
+
+        int[] arr = new int[1000000000];
+
+        for(long i = 0 ; i < 1000000000; i++) {
+            arr[(int) i] = 3/2;
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println( (double) (endTime - stTime) / 1000 );
+    }
+}
