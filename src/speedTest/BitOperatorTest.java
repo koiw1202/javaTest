619c5fb4fa3cfd45dccaddef0b1c1baa819478a7
koiw1202 <koiw1202@naver.com>
2024-12-27 23:28:08 +0900

    연산자 성능 테스트

--- src/speedTest/BitOperatorTest.java
+++ src/speedTest/BitOperatorTest.java
@@ -9,6 +9,7 @@ package speedTest;
  */
 public class BitOperatorTest {
 
+    // 1.529
 //    public static void main(String argsp[]) {
 //
 //        long stTime = System.currentTimeMillis();
@@ -22,6 +23,7 @@ public class BitOperatorTest {
 //        System.out.println( (double) (endTime - stTime) / 1000 );
 //    }
 
+    // 1.533
     public static void main(String argsp[]) {
 
         long stTime = System.currentTimeMillis();
@@ -29,9 +31,11 @@ public class BitOperatorTest {
         int[] arr = new int[1000000000];
 
         for(long i = 0 ; i < 1000000000; i++) {
-            arr[(int) i] = 3/2;
+            arr[(int) i] = 3 >>> 1;
         }
+
         long endTime = System.currentTimeMillis();
         System.out.println( (double) (endTime - stTime) / 1000 );
+
     }
 }
